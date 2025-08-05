c452daf8de602fd94b0e9428f2413b2b8e9eb0c8
Michael3682 <152974233+Michael3682@users.noreply.github.com>
2025-08-05 22:35:32 +0800

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,12 +6,8 @@ export default function Skills() {
             <h2 className="profile-skills--title">Skills</h2>
             <ul className="profile-skills--list">
                 <li className="profile-skill">Web Designing</li>
-                <li className="profile-skill">Empathy</li>
-                <li className="profile-skill">Self-Motivation</li>
-                <li className="profile-skill">Imagination</li>
-                <li className="profile-skill">Patience</li>
-                <li className="profile-skill">Curiosity</li>
-                <li className="profile-skill">Trustworthiness</li>
+                <li className="profile-skill">Roblox Exploiter</li>
+                <li className="profile-skill">Manipulating</li>
             </ul>
         </div>
     )
