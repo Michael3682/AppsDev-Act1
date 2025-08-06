0d40f6cd7dc9ca1c9a68b5e1bbb9c77e36d0ba66
Michael3682 <dominiquedelacerna5@gmail.com>
2025-08-06 20:32:45 +0800

    buttons link updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,8 +5,8 @@ export default function Button() {
         <div className="contact--container">
             <h2 className="contact--title">Contact Me</h2>
             <div className="contact-button--container">
-                <button className="contact-button--facebook" onClick="window.open('https://www.facebook.com/michael.gulfan.12')">Facebook</button>
-                <button className="contact-button--github" onClick="('https://github.com/Michael3682')">Github</button>
+                <button className="contact-button--facebook" onClick={() => window.open('https://www.facebook.com/michael.gulfan.12')}>Facebook</button>
+                <button className="contact-button--github" onClick={() => window.open('https://github.com/Michael3682')}>Github</button>
             </div>
         </div>
     )
